ac719ebe4ae58230f38d24bbc8433fa6bf387584
huygo93123 <huydk1@gmail.com>
2017-09-05 17:11:03 +0700

    add api bitgo

--- app/Http/Controllers/GetNotificationController.php
+++ app/Http/Controllers/GetNotificationController.php
@@ -15,105 +15,24 @@ use Validator;
 use Coinbase\Wallet\Client;
 use Coinbase\Wallet\Configuration;
 use Coinbase\Wallet\Resource\Address;
+use App\BitGoSDK;
 
 class GetNotificationController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
     
     public function getNotification(){
-        // 46345ee6-eff5-5ef5-9fa2-3dc37255dbed
-        // 7e9134cc-1c34-5156-a296-4eba995a51b2
-        // $currentuserid = Auth::user()->id;
-        // $user = UserCoin::findOrFail($currentuserid);
-        // $contact= $client->getAccount($user->accountCoinBase);
-        // $nome = 'name_for_address';
-        // $endereco= new Address(['name' => $nome]);
-        // $client->createAccountAddress($contact, $endereco);
-        // dd($user->accountCoinBase);
-        // dd($currentuserid);
-        $configuration = Configuration::apiKey(config('app.coinbase_key'), config('app.coinbase_secret'));
-        $client = Client::create($configuration);
-
-        // $contaID = 'accountID'
-        // $conta= $client->getAccount("92896cd8-db47-5f21-913b-e4e8dc302bec");
-        // $nome = 'name_for_address';
-        // $endereco= new Address(['name' => $nome]);
-        // $client->createAccountAddress($conta, $endereco);
-
-        $curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.coinbase.com/v2/notifications",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: 8aNbvDlYDlVGvr2o JpLGCFU5OapYUYN3EEy1UxpG75NfrKEq",
-    "cache-control: no-cache"
-  ),
-));
-curl https://api.coinbase.com/v2/notifications \
-  -H 'Authorization: 8aNbvDlYDlVGvr2o JpLGCFU5OapYUYN3EEy1UxpG75NfrKEq'
-$response = curl_exec($curl);
-dd($response);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-
-        // $address = new Address([
-        //     'name' => 'New Address'
-        // ]);
-        // $account = $client->getAccount("3ac91e09-1693-5d4e-adc2-61fee7943c6a");  
-        // $addresses = $client->createAccountAddress($account, $address);
-        // dd($addresses);     
-        // $account = $client->getAccount("3ac91e09-1693-5d4e-adc2-61fee7943c6a");                    
-        // $addressId = $client->getAccountAddresses($account);
-        // dd($addressId);                    
-        // $addresses = $client->getAccountAddress($account, $addressId->getFirstId());                   
-        // $walletAddress = json_encode($addresses->getAddress());
-
-        // $account = $client->getAccount("4361ab31-69a6-52bd-8e18-b91924a7633c");
-        // dd($account);
-        // $account = $client->getAccount($user->accountCoinBase);
-        // $addressId = $client->getAccountAddresses($account);
-
-        // dd($walletAddress);
-        //$addresses = $client->getAccountAddress($account, $addressId->getFirstId());
-        dd($client->getAccounts());
-        // dd($client->getAddresses());
-        // dd($client->getAccountTransactions($account));
-        dd($client->getNotifications());
-
-
-
-        // 28985760-94fa-597d-b8b6-983aad9c804b
-        // if($request->ajax()) {
-        //     if(isset($request['action']) && $request['action'] == 'btc') {
-        //         $currentuserid = Auth::user()->id;
-        //         $user = UserCoin::findOrFail($currentuserid);
-        //         if ($user->walletAddress == '' && $user->accountCoinBase != '') {
-        //             $configuration = Configuration::apiKey(config('app.coinbase_key'), config('app.coinbase_secret'));
-        //             $client = Client::create($configuration);
-        //             $account = $client->getAccount($user->accountCoinBase);
-        //             $addressId = $client->getAccountAddresses($account);
-        //             $addresses = $client->getAccountAddress($account, $addressId->getFirstId());
-        //             $walletAddress = json_encode($addresses->getAddress());
-        //             if ($walletAddress != '') {
-        //                 $user->walletAddress = $walletAddress;
-        //                 $user->save();
-        //             }
-        //         }
-        //         return response()->json(array('walletAddress' => $user->walletAddress));
-        //     }
-        // }
-        // die();
+        // key chains = 33PK7u86q3v1cPqcKBhwmCHzs6RfFYfzJP
+        // wallets_id = 33PK7u86q3v1cPqcKBhwmCHzs6RfFYfzJP
+        // wallets_id = 3JYs1xqtq7fL5dFyy4E3LyeycM5QMwyLkG
+        // address_wallet = 3P5t8x767hbNsc5nkFmHqSiMPVyQ9r4nTu
+       $bitgo = new BitGoSDK('v2x15b687915c114fd8d4583bedcf0617e5b41cfe311d2e31db7aba849939a739ef');
+       $wallet = $bitgo->createWallet();
+       dd($wallet);
+       // dd($bitgo->createAddress('33PK7u86q3v1cPqcKBhwmCHzs6RfFYfzJP','0x69920a31a0a1aca870e2d1bbf03913ad8065f6fb'));
     }
 
 }
