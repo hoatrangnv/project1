0e84694cc626908315818a2baa3b5767622a2231
huynq <huydk1@gmail.com>
2017-09-13 14:11:14 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
