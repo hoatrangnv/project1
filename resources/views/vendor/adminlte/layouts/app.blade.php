9d7d1772601a2dbdfa3e1290f36cc6fbd0d40f5c
Giang <giangitman@gmail.com>
2017-09-19 18:16:01 +0700

    Remove dummy template

--- resources/views/vendor/adminlte/layouts/app.blade.php
+++ resources/views/vendor/adminlte/layouts/app.blade.php
@@ -8,27 +8,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 @section('htmlheader')
     @include('adminlte::layouts.partials.htmlheader')
 @show
-
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
 <body class="skin-blue sidebar-mini">
 <div id="app" v-cloak>
     <div class="wrapper">
