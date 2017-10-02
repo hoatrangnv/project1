816477611257107c940e4a65d9243a6f50077afa
huynq <huydk1@gmail.com>
2017-10-02 15:54:35 +0700

    Update chức năng cập nhật ví BTC vì giờ mỗi user 1 addrees, cùng account

--- app/Notifications/UpdateBtcCoin.php
+++ app/Notifications/UpdateBtcCoin.php
@@ -26,24 +26,28 @@ class UpdateBtcCoin {
         for ($i = 0; $i < ceil(60/$interval); $i++) {
             //Get Notification
             $dataNotApproved = Notification::where("status",0)->get();
-
             //Action
             if( count($dataNotApproved) > 0 ) {
                foreach ($dataNotApproved as $key => $value) {
-
                     $temp = json_decode($value->data);
+                    if(isset($temp->data->address)){
+                        //công vào tk cho user
+                        if(isset(UserCoin::where('walletAddress', $temp->data->address)
+                                ->first()->btcCoinAmount)){
+                            $amountAddress = UserCoin::where('walletAddress', $temp->data->address)
+                                ->first()->btcCoinAmount + $temp->additional_data->amount->amount;
+                            $result = UserCoin::where('walletAddress', $temp->data->address)
+                                   ->update([
+                                       'btcCoinAmount' => 
+                                        $amountAddress
+                                           ]);
 
-                    $result = UserCoin::where('accountCoinBase', $temp->account->id)
-                           ->update([
-                               'btcCoinAmount' => 
-                                $temp->additional_data->amount->amount
-                                   ]);
-
-                    if($result == 1) {
-                        Notification::where("id",$value->id)
-                            ->update(['status' => 1]);
+                            if($result == 1) {
+                                Notification::where("id",$value->id)
+                                    ->update(['status' => 1]);
+                            }
+                        }
                     }
-
                }
             } 
             sleep($interval);
