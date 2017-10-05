f330a6e7b2507327a195b183382382477548e797
huynq <huydk1@gmail.com>
2017-10-05 15:07:10 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -174,6 +174,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        dd($data);
         //Tao acc vi
         try {
             //get userid from uid
