0a17b5098ce59e84ae0d988b43afe2ec5515d43f
Giang <giangitman@gmail.com>
2017-10-06 16:37:46 +0700

    Loi cay nhi phan vo giao dien

--- public/js/jst.js
+++ public/js/jst.js
@@ -4,7 +4,7 @@ this["JST"]["assets/templates/tree-node-leaf.html"] = function(obj) {
     var __t, __p = '',
         __e = _.escape;
     with(obj) {
-        __p += '<div class="node-loyalty node-loyalty-'+loyaltyId+'"<div class="node-name node-lvl-' +
+        __p += '<div class="node-loyalty node-loyalty-'+loyaltyId+'"><div class="node-name node-lvl-' +
             ((__t = (level)) == null ? '' : __t) + '"' + (username == '' ? '' : 'style="border-bottom: 1px solid; color: #3c8dbc"') + '>' + (username == '' ? '' : '<i class="fa fa-user" style="margin-right: 3px; color: gold"></i>') +
             ((__t = (username)) == null ? '' : __t) + '</div>\n<p class="node-title">' +
             ((__t = (pkg)) == null ? '' : __t) + '</p>\n<p class="node-contact">' +
