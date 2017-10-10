de5de99f17116568779936be45dbba89c2aff36d
Giang <giangitman@gmail.com>
2017-10-10 09:53:36 +0700

    Remove auto suggest country phone

--- resources/views/vendor/adminlte/auth/register.blade.php
+++ resources/views/vendor/adminlte/auth/register.blade.php
@@ -144,14 +144,14 @@
         $(document).ready(function(){
             var changurl = changeUrl();
             $("#phone").intlTelInput({
-                initialCountry: "auto",
-                geoIpLookup: function(callback) {
-                    $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
-                    var countryCode = (resp && resp.country) ? resp.country : "";
-                    callback(countryCode);
-                  });
-                },
-                preferredCountries:["vn"]
+                //initialCountry: "auto",
+                // geoIpLookup: function(callback) {
+                //     $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+                //     var countryCode = (resp && resp.country) ? resp.country : "";
+                //     callback(countryCode);
+                //   });
+                // },
+                //preferredCountries:["us"]
             });
             
             $('form').submit(function(){
