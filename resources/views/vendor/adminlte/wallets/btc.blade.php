492b6b2ccc3aebf08de6dbc72dd70e112a844366
Giang <giangitman@gmail.com>
2017-10-10 12:29:51 +0700

    Fix loi layout filter

--- resources/views/vendor/adminlte/wallets/btc.blade.php
+++ resources/views/vendor/adminlte/wallets/btc.blade.php
@@ -43,7 +43,7 @@ use App\Http\Controllers\Wallet\Views\WalletViewController;
     @endif
     <?php echo WalletViewController::viewAllWallet();?>
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-lg-12">
             <!-- Widget: user widget style 1 -->
             <div class="box">
                 <!-- Add the bg color to the header using any of the bg-* classes -->
@@ -68,7 +68,7 @@ use App\Http\Controllers\Wallet\Views\WalletViewController;
                                     <button class="btn bg-olive" data-toggle="modal"
                                             data-target="#buy">{{trans("adminlte_lang::wallet.tranfer_to_clp")}}</button>
                                     <!-- <button class="btn bg-olive" data-toggle="modal"
-                                            data-target="#tranfer">{{trans("adminlte_lang::wallet.transfer")}}</button> -->
+                                            data-target="#tranfer">{{trans("adminlte_lang::wallet.transfer")}}</button>-->
                                 </th>
                             </tr>
                             </tbody>
@@ -84,10 +84,10 @@ use App\Http\Controllers\Wallet\Views\WalletViewController;
         <div class="col-md-12">
             <div class="box">
                 <div class="box-header">
-                    <div class="col-xs-2 no-padding">
+                    <div class="col-xs-6 col-md-2 col-lg-2">
                         {{ Form::select('wallet_type', $wallet_type, ($requestQuery && isset($requestQuery['type']) ? $requestQuery['type'] : 0), ['class' => 'form-control input-sm', 'id' => 'wallet_type']) }}
                     </div>
-                    <div class="col-xs-2">
+                    <div class="col-xs-6 col-md-2 col-lg-2 ">
                         {!! Form::button('Filter', ['class' => 'btn btn-sm btn-primary', 'id' => 'btn_filter']) !!}
                         {!! Form::button('Clear', ['class' => 'btn btn-sm bg-olive', 'id' => 'btn_filter_clear']) !!}
                     </div>
