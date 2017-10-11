3a3314c54a942c9db9801655f0c6ea890c158232
Giang <giangitman@gmail.com>
2017-10-11 09:16:20 +0700

    Title for site

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="{{ url('/favicon.ico') }}" />
-    <title> CLP - @yield('htmlheader_title', 'Your title here') </title>
+    <title> CLP - @yield('htmlheader_title', 'Lending Project') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
