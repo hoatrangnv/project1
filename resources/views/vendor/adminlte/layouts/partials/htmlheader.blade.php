a54b3b417175f44ed94179e4b0ed9c7fba456a7e
Giang <giangitman@gmail.com>
2017-10-12 14:18:39 +0700

    Add google analystic

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -32,4 +32,13 @@
             echo json_encode($trans);
         @endphp
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107989535-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-107989535-1');
+    </script>
 </head>
