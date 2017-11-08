a78347c788f97b883d393efe57920735f36ffa24
Giang <giangitman@gmail.com>
2017-11-09 02:04:51 +0700

    Update term and conditions

--- resources/views/vendor/adminlte/layouts/term_register.blade.php
+++ resources/views/vendor/adminlte/layouts/term_register.blade.php
@@ -1,65 +1,188 @@
 <!DOCTYPE html>
-<!--
-Landing page based on Pratt: http://blacktie.co/demo/pratt/
--->
-<html lang="en">
+<html lang="en" class="full-height">
+
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }} ">
-    <meta name="author" content="Sergi Tur Badenas - acacha.org">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta content="The next generation of on-chain Exchange & Payment Service" name="description" />
+    <meta content="blockchain, exchange network, ethereum, bitcoin, decentralize, free" name="keywords" />
+    <meta content="index, follow" name="robots" />
+    <meta content="CLP Coin" property="og:title" />
+    <meta content="website" property="og:type" />
+    <meta content="Crypto Lending is a decentralized peer-to-peer lending platform that applies Blockchain technology and CLP coin as material.."
+        property="og:description" />
 
-    <meta property="og:title" content="Adminlte-laravel" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }}" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org/" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x600.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x314.png" />
-    <meta property="og:sitename" content="demo.adminlte.acacha.org" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org" />
+    <meta content="The next generation of on-chain Exchange & Payment Service" name="twitter:card" />
+    <meta content="CLP is an open source coin that is written on Blockchain of Ethereum platform and based on ERC20 Standard."
+        name="twitter:description" />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@acachawiki" />
-    <meta name="twitter:creator" content="@acacha1" />
+    <title>CLP Coin</title>
 
-    <title>{{ trans('adminlte_lang::message.landingdescriptionpratt') }}</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('/css/all-landing.css') }}" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="bbh/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- Material Design Bootstrap -->
+    <link href="bbh/css/mdb.css" rel="stylesheet">
 
+    <!-- Your custom styles (optional) -->
+    <link href="bbh/css/style.css" rel="stylesheet">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107989535-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-107989535-1');
+    </script>
 </head>
 
-<body data-spy="scroll" data-target="#navigation" data-offset="50">
+<body class="medical-lp">
 
-<div id="app" v-cloak>
-    <section id="contact" name="contact">
-        <div id="footerwrap">
+    <!--Navigation & Intro-->
+    <header style="height: 30px;">
+
+        <!--Navbar-->
+        <nav class="navbar fixed-top navbar-dark" style="z-index: 100">
+
             <div class="container">
-                <div class="col-lg-12">
-                    <p>
-                        Comming soon!!!
-                    </p>
+
+                <a class="navbar-brand" href="#" style="float: left;">
+                                         <img src="bbh/img/Logo1.png" alt="" class="nav-logo">
+                                    </a>
+                            <a class="navbar-brand" href="#" style="font-size: 35px;">Term and Conditions</a>
+            </div>
+        </nav>
+        <!--/Navbar-->
+
+    </header>
+    <!--/Navigation & Intro-->
+
+    <!--Main content-->
+    <main>
+        <div class="container">
+            <section id="demo" class="section mt-4 mb-4">
+
+                <!--Section heading-->
+                <div class="row">
+                    
+                    <p><strong><em>These Terms and Conditions exist for the benefit of CLP/EPICIRCLES and its Marketing Associates and Investors. CLP/EPICIRCLES, at its sole discretion, reserves the right to terminate the account of any Marketing Associate and Investor caught in violation of the Terms and Conditions without notice.&nbsp;</em></strong><br /> <br /> CLP/EPICIRCLES reserves the right to change these terms and conditions at any time, and you agree to abide by the most recent version of this Terms and Conditions Agreement each time you view and use the Website and/or CLP/EPICIRCLES training &amp; marketing material. Do not use the Website and/or CLP/EPICIRCLES training &amp; marketing material if you do not agree to all of these terms and conditions.</p>
+<p style="padding-left: 30px;"><br /> <strong><em>1. ILLEGAL ACTIVITY.&nbsp;</em></strong><br /> Marketing Associates and Investors may not present CLP/EPICIRCLES promotions on any page, newsgroup, email or any distribution method that is regarded objectionable by CLP/EPICIRCLES or otherwise considered unlawful according to any controlling legal authority.</p>
+<p style="padding-left: 30px;"><br /> <strong><em>2. THIRD-PARTY LIABILITY.&nbsp;</em></strong><br /> CLP/EPICIRCLES will in no way be liable for the actions of third parties that may in any way cause members harm.&nbsp;<br /> <br /> <strong><em>3. CHANGES.&nbsp;</em></strong><br /> CLP/EPICIRCLES reserves the right to update, modify, add or change the terms and conditions without notice.&nbsp;<br /> <br /> <strong><em>4. TERMINATION.&nbsp;</em></strong><br /> The membership of the Marketing Associate and Investor can be canceled anytime by CLP/EPICIRCLES without giving any reason. Invested funds will be returned to investor, deducted a cancellation fee. Non-investments will not be returned; i.e. MLM related fees etc. Marketing Associates and Investors can cancel their account and recurring fees anytime directly from their back office without the assistance of the company.&nbsp;<br /> <br /> <strong><em>5. PAYMENT OF COMMISSION.&nbsp;</em></strong><br /> Commissions is paid only as stated in our Commission Plan.&nbsp;<br /> <br /> <strong><em>6. ETHICS.&nbsp;</em></strong><br /> Any Marketing Associate acting unethical or unprofessional may be removed without refund at the sole discretion of CLP/EPICIRCLES with all future commissions forfeited.&nbsp;<br /> <br /> <strong><em>7. CANCELLATION/TERMINATION OF MEMBERSHIP.&nbsp;</em></strong><br /> Any Marketing Associate and Investor that is canceled/terminated will forfeit all benefits and privileges associated with CLP/EPICIRCLES. Any positions held in commission programs owned by CLP/EPICIRCLES will also be forfeited and ownership will be reassigned to CLP/EPICIRCLES.&nbsp;<br /> <br /> <strong><em>8. RELATIONSHIP OF PARTIES.&nbsp;</em></strong><br /> While the parties shall work hand-in-hand for the benefit of all, the parties acknowledge and agree that the Marketing Associate and Investor shall from a legal perspective, shall act as an independent contractor and not an employee or agent of CLP/EPICIRCLES. Nothing in this Agreement or these Terms and Conditions shall create a partnership, joint venture, agency, or franchise between the parties in the legal sense of these terms. The Marketing Associate and Investor shall not sign any document in the name of or on behalf of CLP/EPICIRCLES nor shall it hold itself out as being an agent of CLP/EPICIRCLES or as having apparent authority to contract for or bind CLP/EPICIRCLES in any way.&nbsp;<br /> <br /> <strong><em>9. DISPUTES.&nbsp;</em></strong><br /> Agreements shall be interpreted under the laws of the country of incorporation for CLP/EPICIRCLES.</p>
+<p style="padding-left: 30px;">&nbsp;<strong><em>10.COPYRIGHT MATERIAL.&nbsp;</em></strong><br /> All branding, logos and graphics contained within CLP/EPICIRCLES are copyright use; distribution or copying of such content is expressly prohibited. CLP/EPICIRCLES provides Marketing Associates and Investors with approved material for marketing. Any 3rd party or self-created marketing material must be first approved by CLP/EPICIRCLES before it may be used.&nbsp;<br /> <br /> <strong><em>11. DAMAGING INTENT.&nbsp;</em></strong><br /> Any Marketing Associate and Investor who engages in chat, email, postings or any other medium, with content that is deemed damaging to CLP/EPICIRCLES and/or its employees and/or fellow Marketing Associates and Investors will be terminated from CLP/EPICIRCLES. Depending on seriousness, CLP/EPICIRCLES may deem it appropriate to exercise legal action.&nbsp;<br /> <br /> <strong><em>12. SPAM.&nbsp;</em></strong><br /> CLP/EPICIRCLES strictly prohibits the use of SPAM. This enforcement is at the sole discretion of CLP/EPICIRCLES and for the benefit of all Marketing Associate and Investors. Marketing Associates and Investors proven to be participating in such activities will have their account(s) terminated and they will forfeit any and all current andfuture commissions.&nbsp;<br /> <br /> <strong><em>13. CONTACT INFORMATION.&nbsp;</em></strong><br /> It is the responsibility of Marketing Associates and Investors and to keep their personal records up to date incl. KYC documents. CLP/EPICIRCLES will not be responsible for communication errors due to incorrect or out of date contact information. Continued failed attempts to make contact with you may result in the termination of your account.&nbsp;<br /> <br /> <strong><em>14. TAX.&nbsp;</em></strong><br /> You, the Marketing Associate and Investor, are responsible for any and all taxes payable in your resident domicile or jurisdiction, for any income and return of investment you recieve from CLP/EPICIRCLES.&nbsp;<br /> <br /> <strong><em>15. NO WARRANTY.&nbsp;</em></strong><br /> All trainings and marketing material etc. is provided "As Is" and without any warranty, express, implied or otherwise, regarding its accuracy or performance.&nbsp;<br /> <br /> <strong><em>16. NO ASSIGNMENT.&nbsp;</em></strong><br /> Marketing Associates and Investors may not assign or transfer any rights or obligations under this Agreement without the prior written consent of CLP/EPICIRCLES.&nbsp;<br /> <br /> <strong><em>17. SEVERABILITY.&nbsp;</em></strong><br /> Marketing Associates and Investors and CLP/EPICIRCLES agree that if any portion of this agreement is found illegal or unenforceable, that portion shall be severed and the remainder of the agreement shall be given full force and effect.&nbsp;<br /> <br /> <strong><em>18. LIMITATION OF LIABILITY; SOLE AND EXCLUSIVE REMEDY; INDEMNIFICATION.&nbsp;</em></strong><br /> You agree that CLP/EPICIRCLES is NOT responsible for the success or failure of your business or your business decisions relating to any information presented by our company or our company products and/or services.&nbsp;<br /> <br /> <strong><em>19. ACCURATENESS OF INFORMATION.&nbsp;</em></strong><br /> CLP/EPICIRCLES makes no representations as to the accurateness or reliability of any advertising message or marketing tool offered through any outside company or on this site. While we believe the people/companies we work with to be honest and ethical, we recommend you do your own due diligence relating to any specific claims or promises made in the marketing message, as it relates to your own use and marketing.&nbsp;<br /> <br /> <strong><em>20. CROSS SPONSORING/CROSS RECRUITING&nbsp;</em></strong><br /> Cross sponsoring, cross recruiting and cross line jumping are prohibited. "Cross sponsoring" means soliciting a Marketing Associate and Investor or any closely related person or entity into a downline different from the existing downline for that Marketing Associate, or a closely related person or entity. "Cross jumping" means an Marketing Associated and Investor or any closely related person or entity voluntarily taking a business that is not in the same downline as the one in which the Marketing Associate and Investor first was placed. "Closely related person or entity" is any person in the household of the Marketing Associate and Investor [e.g. Spouse, Son, Daughter, Parents living in the same household] or any Legal entity which is controlled by the Marketing Associate and Investor.&nbsp;<br /> <br /> <strong><em>21. INCOME REPRESENTATION&nbsp;</em></strong><br /> Marketing Associates and Investors shall not make any income representations except those set forth herein or otherwise specifically set forth in<br /> official company material.&nbsp;</p>
                 </div>
+            </section>
+        </div>
+
+
+
+
+
+
+    </main>
+    <!--/Main content-->
+
+    <!--Footer-->
+    <footer class="page-footer center-on-small-only">
+
+        <!--Footer Links-->
+        <div class="container-fluid">
+
+
+
+
+
+            <!--Second row-->
+            <div class="row mt-2 mb-1 text-center wow fadeIn" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeIn;">
+
+
+
+                <hr class="w-100 hidden-md-up">
+
+                <!--Second column-->
+                <div class="col-xl-6 col-md-6">
+
+                    <!--Title-->
+                    <h5 class="title">Location</h5>
+
+                    <!--Address-->
+                    <p>Hamad Bin Abdulla Rd - Fujairah - UAE</p>
+                    <p>Email: <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=support@cryptolending.org&tf=1">support@cryptolending.org</a></p>
+                    <!--Title-->
+                    <!-- <h5 class="title">Contact</h5> -->
+
+                    <!--Contact info-->
+                    <!-- <p>Mr. Danny Smith + 01 234 567 89 info@gmail.com
+                    </p> -->
+
+                </div>
+                <!--/Second column-->
+
+                <hr class="w-100 hidden-md-up">
+
+                <!--Third column-->
+                <div class="col-xl-6 col-md-6">
+
+                    <!--Title-->
+                    <h5 class="title">Stay in touch</h5>
+
+                    <!--Social buttons-->
+                    <ul class="inline-ul">
+                        <li><a class="btn-floating btn-small btn-fb waves-effect waves-light" href="https://www.facebook.com/CLPcoin-125929678066347/"><i class="fa fa-facebook"> </i></a></li>
+                        <li><a class="btn-floating btn-small btn-tw waves-effect waves-light" href="https://twitter.com/CLPcoins?lang=de"><i class="fa fa-twitter"> </i></a></li>
+
+                    </ul>
+                    <!--/Social buttons-->
+
+                </div>
+                <!--/Third column-->
 
             </div>
-        </div>
-    </section>
-    <footer>
-        <div id="c">
-            <div class="container">
-                <p>
-                    <strong>Copyright &copy; 2017 <a href="{{ URL::to("/") }}">CLP</a>
-                </p>
+            <!--/Second row-->
 
+        </div>
+        <!--/Footer Links-->
+
+        <!--Copyright-->
+        <div class="footer-copyrightclp wow fadeIn" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeIn;">
+            <div class="container-fluid">
+                © 2017 Copyright: <a href="http://www.clpcoin.co"> clpcoin.co </a>
             </div>
         </div>
+        <!--/Copyright-->
+
     </footer>
+    <!--/Footer-->
 
-</div>
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="bbh/js/jquery-3.1.1.min.js"></script>
+
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="bbh/js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="bbh/js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="bbh/js/mdb.min.js"></script>
+
+    <script>
+        //Animation init
+        new WOW().init();
+
+        //Modal
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#contact').focus()
+        })
+    </script>
+</body>
 
 </body>
+
 </html>
