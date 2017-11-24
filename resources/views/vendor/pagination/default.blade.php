a63edb33afcbb7f46f8315adcb0e1c4a61436843
Giang <giangitman@gmail.com>
2017-11-24 14:50:39 +0700

    Change the way display pagination

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,36 +1,32 @@
-@if ($paginator->hasPages())
+<?php
+// config
+$link_limit = 6; // maximum number of links (a little bit inaccurate, but will be ok for now)
+?>
+@if ($paginator->lastPage() > 1)
     <ul class="pagination">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="disabled"><span>&laquo;</span></li>
-        @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-        @endif
-
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li class="disabled"><span>{{ $element }}</span></li>
+        <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
+            <a href="{{ $paginator->url(1) }}">First</a>
+        </li>
+        @for ($i = 1; $i <= $paginator->lastPage(); $i++)
+            <?php
+            $half_total_links = floor($link_limit / 2);
+            $from = $paginator->currentPage() - $half_total_links;
+            $to = $paginator->currentPage() + $half_total_links;
+            if ($paginator->currentPage() < $half_total_links) {
+                $to += $half_total_links - $paginator->currentPage();
+            }
+            if ($paginator->lastPage() - $paginator->currentPage() < $half_total_links) {
+                $from -= $half_total_links - ($paginator->lastPage() - $paginator->currentPage()) - 1;
+            }
+            ?>
+            @if ($from < $i && $i < $to)
+                <li class="{{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
+                    <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                </li>
             @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="active"><span>{{ $page }}</span></li>
-                    @else
-                        <li><a href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-        @else
-            <li class="disabled"><span>&raquo;</span></li>
-        @endif
+        @endfor
+        <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
+            <a href="{{ $paginator->url($paginator->lastPage()) }}">Last</a>
+        </li>
     </ul>
 @endif
